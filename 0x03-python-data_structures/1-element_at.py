9742ab4754a3aaba7c4e1be391dcfc1ef9790b7b
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-06-06 17:31:41 +0300

    New files

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """Retrive an element from a list."""
+    if dx < 0 or dx > (len(my_list) -1):
+        return None
+    return (my_list[dx])
