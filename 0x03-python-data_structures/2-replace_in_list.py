6de762d7b5a645298e49a39af941b94e527fbcc2
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-06-06 18:13:47 +0300

    deletion

--- 0x03-python-data_structures/2-replace_in_list.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if 0 <= idx < lem(my_list):
-        my_list[idx] = element
-        return(my_list)
-    else:
-        return(my_list)
