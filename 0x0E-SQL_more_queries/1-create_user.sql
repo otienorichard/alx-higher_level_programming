530060ec54089f76d21f1fcc5ef22f70775c8add
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-08-11 17:20:55 +0300

    Adding the user file

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- create new user user_0d_1
+-- pwd: user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
