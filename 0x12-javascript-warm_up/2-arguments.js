b4d9fdfe0c8ad858c288c256ea3cf11539e4d76a
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-09-05 15:45:19 +0300

    Another commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const arguments = (message) => {
+    if (message === arguments) {
+        console.log(message);
+    } else {
+        console.log('No argument');
+    }
+};
+arguments();
