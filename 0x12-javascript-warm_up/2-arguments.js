e463dcfe3a6e21508d5e3b44ad591686b81a3578
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-09-05 16:12:59 +0300

    updating the file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-switch(ProcessingInstruction.argv.lengt){
-    case 2:
-        console.log('No argument');
-        break;
-    case 3:
-        console.log('Argument found');
-        break;
-    default:
-        console.log('Arguments found');
+switch (process.argv.length) {
+  case 2:
+    console.log('No argument');
+    break;
+  case 3:
+    console.log('Argument found');
+    break;
+  default:
+    console.log('Arguments found');
 }
